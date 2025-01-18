797f066217ec69d3f8c1d2396b84487a8c456977
elsakurien <elsatechzone@gmail.com>
2025-01-18 17:41:50 +0530

    Update Helloworld.java

--- src/main/java/Helloworld.java
+++ src/main/java/Helloworld.java
@@ -1,6 +1,6 @@
 public class Helloworld {
     public static void main(String[] args) {
         // Print "Hello, World!" to the console
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! edit to know whether auto update");
     }
 }
